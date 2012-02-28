956cfbc2be2207ba7c0c5aa8c43201a3a82970c8
Soushi Hirono <hirono.soushi@f.mbox.nagoya-u.ac.jp>
2012-02-28 20:39:15 +0900

    delete 10_input.el on univ-pc

--- make_symlink.sh
+++ make_symlink.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
+
+# create symbolic link
 cd $(dirname $0)
 for dotfile in .?*
-do
-if [ $dotfile != '..' ] && [ $dotfile != '.git' ]
+do    
+    if [ $dotfile != '..' ] && [ $dotfile != '.git' ]
     then
-ln -Fis "$PWD/$dotfile" $HOME
+	ln -Fis "$PWD/$dotfile" $HOME
     fi
 done
+
+# delete ibus-setting if use univ-server
+if [ "$(whoami | md5sum)" = "d8ee4cf7ae53d63f7fb4ae83082a6bf7  -" ]
+then
+    echo "delete ~/.emacs.d/inits/10_input.el"
+    unlink ~/.emacs.d/inits/10_input.el
+fi
