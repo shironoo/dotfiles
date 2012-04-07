32e2eb01ba53c515dd9a714fa9c9bd4ef31b3402
Soushi Hirono <growupboys2118@gmail.com>
2012-04-08 06:05:20 +0900

    disable elscreen

--- .emacs.d/inits/10_elscreen.el
+++ .emacs.d/inits/10_elscreen.el
@@ -1,2 +1,28 @@
-(setq elscreen-prefix-key "\C-t")
-(require 'elscreen)
+;; prefixキーの設定
+;; (setq elscreen-prefix-key "\C-t")
+
+;; tabを非表示
+;; (setq elscreen-display-tab nil)
+
+;; frame-titleにスクリーンの一覧を表示する
+;; (defun elscreen-frame-title-update ()
+;;   (when (elscreen-screen-modified-p 'elscreen-frame-title-update)
+;;     (let* ((screen-list (sort (elscreen-get-screen-list) '<))
+;;        (screen-to-name-alist (elscreen-get-screen-to-name-alist))
+;;        (title (mapconcat
+;;            (lambda (screen)  
+;;            (format "%d%s %s"
+;;                  screen (elscreen-status-label screen)
+;;                  (get-alist screen screen-to-name-alist)))
+;;            screen-list " ")))
+;;       (if (fboundp 'set-frame-name)
+;;       (set-frame-name title)
+;;     (setq frame-title-format title)))))
+ 
+;; (eval-after-load "elscreen"
+;;   '(add-hook 'elscreen-screen-update-hook 'elscreen-frame-title-update))
+
+;; (require 'elscreen)
+;; (require 'elscreen-server nil t)
+
+
