a314dd59798d4300c5f4e839824301f15b0772c6
Soushi Hirono <growupboys2118@gmail.com>
2012-04-08 06:04:55 +0900

    use original prefix key

--- .emacs.d/inits/10_dmacro.el
+++ .emacs.d/inits/10_dmacro.el
@@ -1,5 +1,5 @@
 ;; マクロの利用
-;(defconst *dmacro-key* "\C-t" "繰返し指定キー")
-(defconst *dmacro-key* "\C-u" "繰返し指定キー")
+(defconst *dmacro-key* "\C-t" "繰返し指定キー")
+;(defconst *dmacro-key* "\C-u" "繰返し指定キー")
 (global-set-key *dmacro-key* 'dmacro-exec)
 (autoload 'dmacro-exec "dmacro" nil t)
