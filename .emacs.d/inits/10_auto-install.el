101fcb83fac343f34f152dd873791c9f724de527
Soushi Hirono <growupboys2118@gmail.com>
2012-04-09 04:24:35 +0900

    change auto-install setting

--- .emacs.d/inits/10_auto-install.el
+++ .emacs.d/inits/10_auto-install.el
@@ -1,5 +1,5 @@
 ;; auto-install
-;(require 'auto-install)
-;(setq auto-install-directory "~/.emacs.d/elisp/")
-;(auto-install-update-emacswiki-package-name t)
-;(auto-install-compatibility-setup)             ; 互換性確保
+(require 'auto-install)
+(setq auto-install-directory "~/.emacs.d/elisp/")
+(auto-install-update-emacswiki-package-name t)
+(auto-install-compatibility-setup)             ; 互換性確保
