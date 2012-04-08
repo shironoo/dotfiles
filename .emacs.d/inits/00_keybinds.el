5a39927b24b7762daf89ce3eee289d37eb25b046
Soushi Hirono <growupboys2118@gmail.com>
2012-04-09 04:23:11 +0900

    change other-buffer key

--- .emacs.d/inits/00_keybinds.el
+++ .emacs.d/inits/00_keybinds.el
@@ -1,10 +1,5 @@
 ;; C-hをBSにバインド
 (global-set-key "\C-h" 'backward-delete-char)
 
-;; C-,で次のバッファへ切り替え
-(global-set-key [?\C-,] 'other-window)
-;; C-.で前のバッファへ切り替え
-(defun back-window ()
-  (interactive)
-  (other-window -1))
-(global-set-key [?\C-.] 'back-window)
+;; bufferの切り替え
+(global-set-key (kbd "C-o") 'other-window)
