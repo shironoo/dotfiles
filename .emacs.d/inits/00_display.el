319b1acb48b04359ddd5b2e4527b235090bdc3f7
Soushi Hirono <growupboys2118@gmail.com>
2012-04-09 04:21:51 +0900

    modify parent-view

--- .emacs.d/inits/00_display.el
+++ .emacs.d/inits/00_display.el
@@ -22,5 +22,14 @@
 (setq inhibit-startup-message t)
 
 ;; 対応する括弧を強調
-(show-paren-mode t)
-(put 'set-goal-column 'disabled nil)
+(setq show-paren-delay 0) ; 表示までの秒数
+(show-paren-mode t) ; 有効化
+;; parenのスタイル
+(setq show-paren-style 'parenthesis)
+;(setq show-paren-style 'expression)
+;; フェイスを変更する
+;(set-face-background 'show-paren-match-face nil)
+;(set-face-underline-p 'show-paren-match-face "yellow")
+
+;; タイトルバーにファイルのフルパスを表示
+(setq frame-title-format "%f")
