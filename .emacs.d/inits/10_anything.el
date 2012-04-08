35d6ea12da25155dd4963b5dc7c06c4119e93452
Soushi Hirono <growupboys2118@gmail.com>
2012-04-09 04:52:02 +0900

    enable incremental search

--- .emacs.d/inits/10_anything.el
+++ .emacs.d/inits/10_anything.el
@@ -1,32 +1,35 @@
 ;; anything
-(when (require 'anything nil t)
-  (setq
-   anything-idle-delay 0.3
-   anything-input-idle-delay 0.2
-   anything-candidate-number-limit 100
-   anything-quick-update t
-   anything-enable-shortcuts 'alphabet)
+;; (when (require 'anything nil t)
+;;   (setq
+;;    anything-idle-delay 0.3
+;;    anything-input-idle-delay 0.2
+;;    anything-candidate-number-limit 100
+;;    anything-quick-update t
+;;    anything-enable-shortcuts 'alphabet)
 
-  (when (require 'anything-config nil t)
-    ;; rootでの実行コマンド
-    (setq anything-su-or-sudo "sudo"))
+;;   (when (require 'anything-config nil t)
+;;     ;; rootでの実行コマンド
+;;     (setq anything-su-or-sudo "sudo"))
 
-  (require 'anything-match-plugin nil t)
+;;   (require 'anything-match-plugin nil t)
 
-  (when (and (executable-find "cmigemo")
-             (require 'migemo nil t))
-    (require 'anything-migemo nil t))
+;;   (when (and (executable-find "cmigemo")
+;;              (require 'migemo nil t))
+;;     (require 'anything-migemo nil t))
 
-  (when (require 'anything-complete nil t)
-    (anything-lisp-complete-symbol-set-timer 150))
+;;   (when (require 'anything-complete nil t)
+;;     (anything-lisp-complete-symbol-set-timer 150))
 
-  (require 'anything-show-completion nil t)
+;;   (require 'anything-show-completion nil t)
 
-  (when (require 'auto-install nil t)
-    (require 'anything-auto-install nil t))
+;;   (when (require 'auto-install nil t)
+;;     (require 'anything-auto-install nil t))
 
-  (when (require 'descbinds-anything nil t)
-    (descbinds-anything-install)))
+;;   (when (require 'descbinds-anything nil t)
+;;     (descbinds-anything-install)))
+
+(defvar org-directory "")
+(require 'anything-startup) 
 
 ;; AnythingからTAGSを利用しやすくするコマンド作成
 (when (and (require 'anything-exuberant-ctags nil t)
