78d77efcd6986aed48f94d807fdaab0b4eaac072
Soushi Hirono <shirono@net.itc.nagoya-u.ac.jp>
2013-01-12 03:23:04 +0900

    use popwin-yatex.el

--- .emacs.d/inits/10_popwin.el
+++ .emacs.d/inits/10_popwin.el
@@ -1,2 +1,9 @@
-(require 'popwin)
+(require 'popwin-yatex)
+;(require 'popwin)
+
 (setq display-buffer-function 'popwin:display-buffer)
+
+(setq anything-samewindow nil)
+(push '("*anything*" :height 20) popwin:special-display-config)
+
+(push '("*YaTeX-typesetting*") popwin:special-display-config)
