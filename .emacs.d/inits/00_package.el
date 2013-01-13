3dfddcde69b93d5d78969b5a60ba5433c3b1fca7
Soushi Hirono <growupboys2118@gmail.com>
2013-01-14 00:36:07 +0900

    add mepel repository

--- .emacs.d/inits/00_package.el
+++ .emacs.d/inits/00_package.el
@@ -1,8 +1,9 @@
 ;; package.el
 (when (require 'package nil t)
-  ;; リポジトリにMarmaladeとELPAを追加
+  ;; リポジトリにMarmalade,ELPA,MELPAを追加
   (add-to-list 'package-archives
                '("marmalade" . "http://marmalade-repo.org/packages/"))
   (add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/"))
+  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
   ;; パッケージにパスを通してロード
   (package-initialize))
