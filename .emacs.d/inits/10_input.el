fd8a9e08e21f62a779325101beb998308d850da0
Soushi Hirono <growupboys2118@gmail.com>
2013-02-15 19:41:48 +0900

    Use mozc.el

--- .emacs.d/inits/10_input.el
+++ .emacs.d/inits/10_input.el
@@ -1,16 +1,23 @@
 ;; ibus-mode
-(require 'ibus)
+;(require 'ibus)
 ;; Turn on ibus-mode automatically after loading .emacs
-(add-hook 'after-init-hook 'ibus-mode-on)
+;(add-hook 'after-init-hook 'ibus-mode-on)
 ;; Use C-SPC for Set Mark command
-(ibus-define-common-key ?\C-\s nil)
+;(ibus-define-common-key ?\C-\s nil)
 ;; Use C-/ for Undo command
-(ibus-define-common-key ?\C-/ nil)
+;(ibus-define-common-key ?\C-/ nil)
 ;; Change cursor color depending on IBus status
-(setq ibus-cursor-color '("limegreen" "white" "blue"))
+;(setq ibus-cursor-color '("limegreen" "white" "blue"))
 ;; C-\で入力をトグル
-(global-set-key "\C-\\" 'ibus-toggle)
+;(global-set-key "\C-\\" 'ibus-toggle)
 
-;(require 'mozc)
-;(set-language-environment "japanese")
-;(setq default-input-method "japanese-mozc")
+(when (require 'mozc nil t)
+  (setq default-input-method "japanese-mozc")
+  (setq mozc-candidate-style 'overlay)
+
+  ;; faces
+  (set-face-attribute 'mozc-cand-overlay-even-face 'nil
+                      :background "aquamarine" :foreground "black")
+  (set-face-attribute 'mozc-cand-overlay-odd-face 'nil
+                      :background "aquamarine" :foreground "black"))
+(global-set-key "\C-\\" 'toggle-input-method)
