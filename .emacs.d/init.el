52d1485052f28d27d69030478d2a55711fd7ae86
Soshi Hirono <growupboys2118@gmail.com>
2013-02-17 15:51:26 +0900

    use default theme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -34,6 +34,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("42c7f1aa7a3db4ab8efabf1d10c158c2f1414ac57cb207cde815eadad72170d2" "6cfe5b2f818c7b52723f3e121d1157cf9d95ed8923dbc1b47f392da80ef7495d" "e9a1226ffed627ec58294d77c62aa9561ec5f42309a1f7a2423c6227e34e3581" "211bb9b24001d066a646809727efb9c9a2665c270c753aa125bace5e899cb523" default)))
+ '(custom-theme-directory "~/.emacs.d/themes/")
  '(safe-local-variable-values (quote ((TeX-master . "../graduation_thesis") (TeX-master . "t") (TeX-master . "graduation_thesis")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
---
+++ .emacs.d/inits/10_theme.el
@@ -0,0 +1,8 @@
+(custom-set-variables
+ '(custom-theme-directory "~/.emacs.d/themes/")
+)
+
+(load-theme 'tango-2 t)
+(enable-theme 'tango-2)
+;(load-theme 'molokai t)
+;(enable-theme 'molokai)
