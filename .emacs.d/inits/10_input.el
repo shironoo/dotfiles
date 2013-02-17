2f2257aba99daee4b02cc3ad91cf523605265b02
Soshi Hirono <growupboys2118@gmail.com>
2013-02-17 15:57:06 +0900

    change color setting

--- .emacs.d/inits/10_input.el
+++ .emacs.d/inits/10_input.el
@@ -1,23 +1,39 @@
-;; ibus-mode
-;(require 'ibus)
-;; Turn on ibus-mode automatically after loading .emacs
-;(add-hook 'after-init-hook 'ibus-mode-on)
-;; Use C-SPC for Set Mark command
-;(ibus-define-common-key ?\C-\s nil)
-;; Use C-/ for Undo command
-;(ibus-define-common-key ?\C-/ nil)
-;; Change cursor color depending on IBus status
-;(setq ibus-cursor-color '("limegreen" "white" "blue"))
-;; C-\で入力をトグル
-;(global-set-key "\C-\\" 'ibus-toggle)
+(global-set-key "\C-\\" 'toggle-input-method)
 
 (when (require 'mozc nil t)
+  (set-language-environment "japanese")
   (setq default-input-method "japanese-mozc")
   (setq mozc-candidate-style 'overlay)
+  ;(setq mozc-color "blue")
+
+(setq mozc-color "SeaGreen4")
+(setq mozc-off-color "PaleGreen3")
+
+(defun mozc-change-cursor-color ()
+  (if mozc-mode
+      (set-cursor-color mozc-color)
+    (set-cursor-color mozc-off-color)))
+
+(add-hook 'input-method-activate-hook
+          (lambda () (mozc-change-cursor-color)))
+(add-hook 'input-method-inactivate-hook
+          (lambda () (mozc-change-cursor-color)))
 
   ;; faces
+(setq color1 "#5f005f")
+(setq color2 "#ffaf00")
+(setq color3 "#5f00d7")
+(setq color4 "brightwhite")
   (set-face-attribute 'mozc-cand-overlay-even-face 'nil
-                      :background "aquamarine" :foreground "black")
+                      :background color1 :foreground color4)
   (set-face-attribute 'mozc-cand-overlay-odd-face 'nil
-                      :background "aquamarine" :foreground "black"))
-(global-set-key "\C-\\" 'toggle-input-method)
+                      :background color1 :foreground color4)
+  (set-face-attribute 'mozc-cand-overlay-focused-face 'nil
+                      :background color2 :foreground "#5f0000")
+  (set-face-attribute 'mozc-cand-overlay-footer-face 'nil
+                      :background color3 :foreground "#5f00ff")
+;  (set-face-attribute 'mozc-cand-overlay-even-face 'nil
+;                      :background "aquamarine" :foreground "black")
+;  (set-face-attribute 'mozc-cand-overlay-odd-face 'nil
+;                      :background "aquamarine" :foreground "black")
+)
